6eb09b70a4635a50e3cc96a739643b1e7c2b1292
riaanggreni <riaanggreni34@gmail.com>
2022-06-14 16:41:46 +0800

    first commit

---
+++ src/components/Todo.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import TodoForm from "./TodoForm";
+import { RiCloseCircleLine } from "react-icons/ri";
+//import { VscEdit } from "react-icons/ri";
+
+function Todo({ todos, completeTodo, removeTodo }) {
+  const [edit, setEdit] = useState({
+    id: null,
+    value: "",
+  });
+
+  return todos.map((todo, index) => (
+    <div className={todo.isComplite ? "todo-row complete" : "todo-row"} key={index}>
+      <div key={todo.id} onClick={() => completeTodo(todo.id)}>
+        {todo.text}
+      </div>
+      <div className="icons">
+        <RiCloseCircleLine onClick={() => removeTodo(todo.id)} className="delete-icon" />
+        {/* <VscEdit /> */}
+      </div>
+    </div>
+  ));
+}
+
+export default Todo;
